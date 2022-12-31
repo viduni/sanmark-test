6f566c779ad39a7d713d5c000c32bc08b710dad1
Viduni Randima <mahaduravidur@gmail.com>
2022-12-31 19:30:42 +0530

    3

--- src/resources/views/note/index.blade.php
+++ src/resources/views/note/index.blade.php
@@ -15,10 +15,33 @@
                 <span class="block mt-3 text-dark-grey">{!!session('message')['description']!!}</span>
                 @endif
                 
-                <div>
-                    @php
-                    var_dump($notes);
-                    @endphp
+                <div class="p-6 text-gray-900">
+                <table class="table">
+                    <thead>
+                        <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Description</th>
+                        <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($notes as $note)
+                        <tr>
+                        <td>{{$note['id']}}</td>
+                        <td>{{$note['title']}}</td>
+                        <td>{{$note['description']}}</td>
+                        <td>
+                            <div class="grid grid-cols-2">
+                            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" /></svg>
+                            <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" /></svg>
+
+                            </div>
+                        </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
                 </div>
             </div>
         </div>
