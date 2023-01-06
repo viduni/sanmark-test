9e5af7fdb59e94568fd31282fe4e359de72cea92
Viduni Randima <mahaduravidur@gmail.com>
2023-01-06 09:57:23 +0530

    add migrations

---
+++ src/database/migrations/2023_01_06_041431_create_readings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('readings', function (Blueprint $table) {
+            $table->id();
+            $table->date('reading_date');
+            $table->integer('reading_value');
+            $table->integer('customer_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('readings');
+    }
+};
